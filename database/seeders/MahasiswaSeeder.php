318aab5b154c063d96e2b487c49634d0a2629e8b
jiwatmikapratama <jiwatmikapratama@gmail.com>
2022-10-21 17:27:06 +0800

    Membuat seeder mahasiswa

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Mahasiswa;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,9 @@ class MahasiswaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Mahasiswa::create([
+            'nama_mahasiswa' => 'I Putu Pratama Putra Jiwatmika',
+            'prodi' => 'S1 Pendidikan Teknik Informatika',
+        ]);
     }
 }
