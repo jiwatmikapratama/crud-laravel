cea1be36cb7b4a79fcf2426c93af18e517777bcd
jiwatmikapratama <jiwatmikapratama@gmail.com>
2022-10-21 17:22:28 +0800

    Membuat view dan migration

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.main')
+@section('container')
+    <table class="table mt-4">
+        <thead>
+            <tr>
+                <th scope="col">No.</th>
+                <th scope="col">Nama</th>
+                <th scope="col">NIM</th>
+                <th scope="col">Prodi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
